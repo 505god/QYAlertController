166955e18a0a8de2319cc1d2a9bca8028aad2466
qiu <18915410342@126.com>
2017-08-07 16:29:14 +0800

    Initial Commit

---
+++ QYAlertController/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QYAlertController
+//
+//  Created by 邱成西 on 2017/8/7.
+//  Copyright © 2017年 邱大侠. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
